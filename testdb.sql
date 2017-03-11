81ede6e0d29321f8c66259bdfa62dc2cb7c30028
inkersgithub <inkers.info@gmail.com>
2017-03-11 19:07:27 +0530

    fixed db missing issue

--- testdb.sql
+++ testdb.sql
@@ -1,11 +1,118 @@
-CREATE DATABASE `testdb`;
-USE `testdb`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 11, 2017 at 02:35 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentclass`
+--
+
+CREATE TABLE `studentclass` (
+  `sn` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `classname` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacherclass`
+--
+
+CREATE TABLE `teacherclass` (
+  `sn` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `classname` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(8) NOT NULL,
   `name` varchar(30) NOT NULL,
   `email` varchar(60) NOT NULL,
   `password` varchar(40) NOT NULL,
-  `type` varchar(7) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `type` varchar(7) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `type`) VALUES
+(1, 'Zade', 'zade@gmail.com', 'e19d5cd5af0378da05f63f891c7467af', 'teacher'),
+(2, 'Navaneetha', 'navaneetha.sree@gmail.com', 'e19d5cd5af0378da05f63f891c7467af', 'student');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  ADD PRIMARY KEY (`sn`),
+  ADD UNIQUE KEY `email` (`email`,`classname`);
+
+--
+-- Indexes for table `teacherclass`
+--
+ALTER TABLE `teacherclass`
+  ADD PRIMARY KEY (`sn`),
+  ADD UNIQUE KEY `email` (`email`,`classname`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`),
+  ADD UNIQUE KEY `email_3` (`email`),
+  ADD KEY `email_4` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `studentclass`
+--
+ALTER TABLE `studentclass`
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacherclass`
+--
+ALTER TABLE `teacherclass`
+  MODIFY `sn` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
