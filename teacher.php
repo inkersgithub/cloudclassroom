6b582bf7079f126ee2b06cab341e4172eb374b62
inkersgithub <inkers.info@gmail.com>
2017-03-13 23:20:24 +0530

    added popup for new class creation

--- teacher.php
+++ teacher.php
@@ -9,6 +9,7 @@ if($_SESSION['usr_type']!="teacher" OR isset($_SESSION['usr_id'])==""){
     header("Location: index.php");
   }
 }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -43,6 +44,115 @@ if($_SESSION['usr_type']!="teacher" OR isset($_SESSION['usr_id'])==""){
 		</div>
 	</div>
 </nav>
+
+<style>
+/* new class button style */
+.newbutton
+{
+  width: 150px;
+  text-align: center;
+  margin:50%;
+
+}
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+/* The Close Button */
+.close {
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
+
+<button id="newclass" class="btn btn-primary" >Create new class</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <div class="row">
+  		<div class="col-md-4 col-md-offset-4 well">
+  			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="newclassform">
+  				<fieldset>
+  					<legend>Create new classroom</legend>
+
+  					<div class="form-group">
+  						<label for="name">Enter new class name</label>
+  						<input type="text" name="newclassname" placeholder="class name" required class="form-control" />
+  					</div>
+
+  					<div class="form-group">
+  						<input type="submit" name="createclass" value="Submit" class="btn btn-primary" />
+  					</div>
+  				</fieldset>
+  			</form>
+  			<span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+  		</div>
+  	</div>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("newclass");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
 
 <script src="js/jquery-1.10.2.js"></script>
 <script src="js/bootstrap.min.js"></script>
